b1491f9cae92bdf08688bd3d237b8731841ba729
Francisco Jiménez López <93322694+franjimlop@users.noreply.github.com>
2023-04-27 12:58:32 +0200

    Información mostrada
    Ya se ven todos los pokemon con su información (nombre, foto, tipo, altura y peso)

---
+++ app.js
@@ -0,0 +1,53 @@
+// Constante que guarda la URL base de la PokeAPI
+const url = 'https://pokeapi.co/api/v2/';
+
+// Función que devuelve una promesa que se resuelve con todos los Pokemon
+async function obtenerPokemon() {
+    // Hacemos una solicitud para obtener la cantidad total de Pokemon
+    const response = await fetch(`${url}pokemon/`);
+    const data = await response.json();
+    const totalPokemon = data.count;
+
+    // Calculamos la cantidad de páginas necesarias para obtener todos los Pokemon
+    const paginas = Math.ceil(totalPokemon / 20);
+
+    // Hacemos una solicitud para cada página y combinamos los resultados
+    const resultados = [];
+    for (let i = 1; i <= paginas; i++) {
+        const response = await fetch(`${url}pokemon/?limit=20&offset=${(i - 1) * 20}`);
+        const data = await response.json();
+        resultados.push(...data.results);
+    }
+
+    // Hacemos una solicitud para cada Pokemon para obtener su información completa
+    const pokemon = await Promise.all(resultados.map(async (resultado) => {
+        const response = await fetch(resultado.url);
+        return response.json();
+    }));
+
+    return pokemon;
+}
+
+// Llamamos a la función y creamos nuestras tarjetas de Pokemon
+obtenerPokemon().then((pokemon) => {
+    const resultados = document.querySelector("#resultados");
+    resultados.innerHTML = '';
+
+    // Creamos una tarjeta para cada Pokemon
+    pokemon.map((item) => {
+        const cardItem = document.createElement('div');
+        cardItem.classList.add('col', 'col-md-3', 'col-sm-1');
+        cardItem.innerHTML = `
+        <div class="card">
+        <h2>${item.name}</h2>
+        <img src="${item.sprites.front_default}">
+        <p>ID: ${item.id}</p>
+        <p>Tipo: ${item.types.map(type => type.type.name).join(", ")}</p>
+        <p>Altura: ${item.height / 10} m</p>
+        <p>Peso: ${item.weight} kg</p>
+        </div>`;
+        //puedo añadir abilities y moves y poner todos los sprites
+        resultados.appendChild(cardItem);
+    });
+    console.log(pokemon);
+});
